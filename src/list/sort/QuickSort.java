45b3fd1678c74ef78b4af1e77a136a5def3438d2
chenwang <chenwang@bixin.cn>
2023-09-20 14:36:17 +0800

    提取通用能力至util

--- src/list/sort/QuickSort.java
+++ src/list/sort/QuickSort.java
@@ -6,7 +6,7 @@ import list.util.Util;
 
 public class QuickSort {
     public static void main(String[] args) {
-        ListNode head = ListGenerator.build(new Integer[] { 2, 12, 1, 0, 5, 6 });
+        ListNode head = ListGenerator.build(new Integer[] { 2, 12, 1, 10, 5, 6 });
         QuickSort test = new QuickSort();
         System.out.println(test.quickSort(head));
     }
@@ -17,14 +17,16 @@ public class QuickSort {
         }
 
         // 获取中间结点
-        ListNode mid = middleNode(head);
+        ListNode mid = Util.middleNode(head);
 
         ListNode rightHead = mid.next;
+
+        mid.next = null;
+
+        // 当只有两个节点时的处理
         if (rightHead == null) {
             rightHead = mid;
             head.next = null;
-        } else {
-            mid.next = null;
         }
 
         // 左链表排序
@@ -33,15 +35,5 @@ public class QuickSort {
         ListNode right = quickSort(rightHead);
         // 合并排序
         return Util.mergeTwoLists(left, right);
-    }
-
-    private ListNode middleNode(ListNode head) {
-        ListNode slow = head;
-        ListNode fast = head;
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-        return slow;
     }
 }
